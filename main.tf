3ce3a6658e5c1c5100268a3d610e306b9d2ed305
cmomodo <ceesay.ml@outlook.com>
2024-07-30 09:26:29 +0100

    First initial commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a new S3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "www.devorderz.com"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+# Set public access block configuration
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+# Set bucket policy to make it publicly accessible
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Statement1",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::www.devorderz.com/*"
+        }
+    ]
+}
+POLICY
+}
