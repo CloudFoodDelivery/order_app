da43ab30acd07580942c0b92f3c2a329df332954
Dadrion <dtuircuit@gmail.com>
2024-08-01 18:58:40 -0500

    updated file with CF and R53 configs. Will plug-in domain name after registration

--- main.tf
+++ main.tf
@@ -12,6 +12,69 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Hosted zone for your Route 53 domain
+resource "aws_route53_zone" "main" {
+  name = "replacethisdomain.com" # Replace with your registered domain
+}
+
+#DNS record for CF CDN
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+ # Defined CF Distrubition 
+resource "aws_cloudfront_distribution" "main" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "myS3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 CloudFront Distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "myS3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
 # Create a new S3 bucket
 resource "aws_s3_bucket" "example" {
   bucket = "www.devorderz.com"
