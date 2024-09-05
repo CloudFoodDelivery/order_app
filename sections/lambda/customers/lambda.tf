7ebf98fe2b06f0de848839bc33531bb3c4b9bc7f
cmomodo <ceesay.ml@outlook.com>
2024-09-05 23:26:56 +0100

    the updated lambda function

--- sections/lambda/customers/lambda.tf
+++ sections/lambda/customers/lambda.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
 # Archive file data source
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda"
+  source_dir  = "${path.module}/lambda_function_customer"
   output_path = "${path.module}/lambda_function.zip"
 }
 
